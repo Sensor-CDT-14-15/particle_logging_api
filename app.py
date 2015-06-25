8a70fdc35c322953a83d4f344ec66c8465787f59
James Manton <ajd.manton@googlemail.com>
2015-06-25 15:20:58 +0100

    Add basic database connection check

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 #!/bin/python
 from flask import Flask
+import MySQLdb as mdb
+
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = ''
+MYSQL_DATABASE = 'particle'
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "Particle logging API"
+	try:
+		con = mdb.connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE)
+		cur = con.cursor()
+		cur.execute("SELECT VERSION()")
+		ver = cur.fetchone()
+		return "Particle logging API. Database version: %s." % ver
+	except:
+		return "Particle logging API"
 
 if __name__ == '__main__':
 	app.run(debug=True)

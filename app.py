5667698d81c7546b2940d8c44511735c662295b2
James Manton <ajd.manton@googlemail.com>
2015-06-25 16:35:59 +0100

    Add endpoint to list all devices we know about in the database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/bin/python
-from flask import Flask
+from flask import Flask, jsonify, make_response
 import MySQLdb as mdb
 
 MYSQL_HOST = 'localhost'
@@ -19,6 +19,20 @@ def index():
 		return "Particle logging API. Database version: %s." % ver
 	except:
 		return "Particle logging API"
+
+@app.route('/particle/devices', methods=['GET'])
+def get_devices():
+	try:
+		con = mdb.connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE)
+		cur = con.cursor()
+		cur.execute("SELECT * FROM devices")
+		rows = cur.fetchall()
+		response = []
+		for row in rows:
+			response.append({"id": row[1], "name": row[2]})
+		return jsonify(devices=response)
+	except:
+		return make_response(jsonify({'error': 'No devices listed.'}), 404)
 
 if __name__ == '__main__':
 	app.run(debug=True)

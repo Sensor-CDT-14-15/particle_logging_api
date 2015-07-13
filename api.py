b00b443c6706f052289c67f855dbd3219f558d3e
James Manton <ajd.manton@googlemail.com>
2015-07-13 10:48:56 +0100

    Add endpoint for event data

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 import MySQLdb as mdb
 
 MYSQL_HOST = 'localhost'
@@ -32,6 +32,22 @@ def get_devices():
 		return jsonify(devices=response)
 	except:
 		return make_response(jsonify({'error': 'No devices listed.'}), 404)
+
+@app.route('/events', methods=['GET'])
+def get_events():
+	start = request.args.get('start')
+	num_rows = request.args.get('num_rows')
+	try:
+		con = mdb.connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE)
+		cur = con.cursor()
+		cur.execute("SELECT * FROM events LIMIT " + str(int(start) - 1) + ", " + num_rows)
+		response = []
+		for i in range(0, int(num_rows)):
+			row = cur.fetchone()
+			response.append({"id": row[0], "device": row[2], "data": row[3]})
+		return jsonify(events=response)
+	except:
+		return make_response(jsonify({'error': 'No event data returned'}), 404)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080)
